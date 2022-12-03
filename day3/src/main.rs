0d28e67b8c7ef32c211db2b7d48cab3daabcdb14
David Stone <david@stabbylambda.com>
2022-12-02 23:44:36 -0800

    Done with day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,35 +3,50 @@ use std::collections::HashSet;
 use common::get_input_strings;
 fn main() {
     let lines = get_input_strings();
+    let rucksacks = get_rucksacks(&lines);
 
-    let score = problem1(&lines);
+    let score = problem1(&rucksacks);
     println!("problem 1 score: {score}");
 
-    let score = problem2(&lines);
+    let score = problem2(&rucksacks);
     println!("problem 2 score: {score}");
 }
 
 #[derive(Debug)]
 struct Rucksack {
-    compartment1: Vec<char>,
-    compartment2: Vec<char>,
+    compartment1: HashSet<char>,
+    compartment2: HashSet<char>,
 }
 
 impl Rucksack {
-    fn get_shared(&self) -> Vec<char> {
-        let s1: HashSet<char> = HashSet::from_iter(self.compartment1.iter().map(|c| *c));
-        let s2: HashSet<char> = HashSet::from_iter(self.compartment2.iter().map(|c| *c));
-        let common: Vec<char> = s1.intersection(&s2).map(|c| *c).collect();
-
-        common
+    fn new(s: &str) -> Rucksack {
+        let (c1, c2) = s.split_at(s.len() / 2);
+        Rucksack {
+            compartment1: HashSet::from_iter(c1.chars()),
+            compartment2: HashSet::from_iter(c2.chars()),
+        }
     }
-}
 
-fn get_rucksack(s: &str) -> Rucksack {
-    let (compartment1, compartment2) = s.split_at(s.len() / 2);
-    Rucksack {
-        compartment1: compartment1.chars().map(|c| c as char).collect(),
-        compartment2: compartment2.chars().map(|c| c as char).collect(),
+    fn get_shared(&self) -> &char {
+        self.compartment1
+            .intersection(&self.compartment2)
+            .next()
+            .unwrap()
+    }
+
+    fn get_all_items(&self) -> HashSet<&char> {
+        self.compartment1.union(&self.compartment2).collect()
+    }
+
+    fn get_badge<'a>(&'a self, other1: &'a Rucksack, other2: &'a Rucksack) -> &char {
+        let s0: HashSet<_> = self.get_all_items();
+        let s1: HashSet<_> = other1.get_all_items();
+        let s2: HashSet<_> = other2.get_all_items();
+
+        let common1: HashSet<_> = s0.intersection(&s1).collect();
+        let common2: HashSet<_> = s1.intersection(&s2).collect();
+
+        common1.intersection(&common2).next().unwrap()
     }
 }
 
@@ -39,60 +54,47 @@ fn prioritize(c: &char) -> u32 {
     (*c as u32) - if c.is_lowercase() { 96 } else { 38 }
 }
 
-fn problem1(lines: &[String]) -> u32 {
-    let rucksacks: Vec<Rucksack> = lines.iter().map(|s| get_rucksack(&s)).collect();
-    let priorities = rucksacks
+fn get_rucksacks(lines: &[String]) -> Vec<Rucksack> {
+    lines
         .iter()
-        .flat_map(|r| {
-            r.get_shared()
-                .iter()
-                .map(|c| prioritize(c))
-                .collect::<Vec<u32>>()
-        })
-        .sum();
-
-    priorities
+        .map(|s| Rucksack::new(&s))
+        .collect::<Vec<Rucksack>>()
 }
 
-fn problem2(lines: &[String]) -> u32 {
-    0
+fn problem1(rucksacks: &[Rucksack]) -> u32 {
+    rucksacks.iter().map(|r| prioritize(r.get_shared())).sum()
+}
+
+fn problem2(rucksacks: &[Rucksack]) -> u32 {
+    rucksacks
+        .chunks(3)
+        .map(|g| {
+            let [g0, g1, g2] = g else {
+                panic!("not a valid group");
+            };
+            prioritize(g0.get_badge(g1, g2))
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod test {
     use common::test::get_input_strings;
 
-    #[test]
-    fn test_get_rucksack() {
-        let r = get_rucksack("vJrwpWtwJgWrhcsFMMfFFhFp");
-        assert_eq!(
-            r.compartment1,
-            "vJrwpWtwJgWr".chars().collect::<Vec<char>>()
-        );
-        assert_eq!(
-            r.compartment2,
-            "hcsFMMfFFhFp".chars().collect::<Vec<char>>()
-        );
-    }
-
-    #[test]
-    fn common_test() {
-        let r = get_rucksack("vJrwpWtwJgWrhcsFMMfFFhFp");
-        assert_eq!(r.get_shared()[0], 'p');
-    }
-
-    use crate::{get_rucksack, problem1, problem2};
+    use crate::{get_rucksacks, problem1, problem2};
     #[test]
     fn first() {
         let lines = get_input_strings();
-        let result = problem1(&lines);
+        let rucksacks = get_rucksacks(&lines);
+        let result = problem1(&rucksacks);
         assert_eq!(result, 157);
     }
 
     #[test]
     fn second() {
         let lines = get_input_strings();
-        let result = problem2(&lines);
-        assert_eq!(result, 0)
+        let rucksacks = get_rucksacks(&lines);
+        let result = problem2(&rucksacks);
+        assert_eq!(result, 70)
     }
 }
