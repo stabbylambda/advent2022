0815006f9164a5bf6db5d5a59461743db6ac5da4
David Stone <david@stabbylambda.com>
2022-12-07 10:00:16 -0800

    Day7 problem 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -10,20 +10,19 @@ use nom::{
 };
 use petgraph::{
     algo::toposort,
-    dot::{Config, Dot},
     graph::NodeIndex,
     Direction::{Incoming, Outgoing},
     Graph,
 };
 fn main() {
     let lines = get_raw_input();
-    let input = parse_lines(&lines);
+    let input = Command::parse_all(&lines);
 
     let score = problem1(&input);
     println!("problem 1 score: {score}");
 
-    // let score = problem2(&lines);
-    // println!("problem 2 score: {score}");
+    let score = problem2(&input);
+    println!("problem 2 score: {score}");
 }
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
@@ -43,6 +42,24 @@ enum Listing<'a> {
     File(Data<'a>),
 }
 
+impl<'a> Listing<'a> {
+    fn parse(s: &str) -> IResult<&str, Vec<Listing>> {
+        separated_list1(
+            line_ending,
+            alt((
+                map(
+                    separated_pair(nom_u32, tag(" "), not_line_ending),
+                    |(size, name)| Listing::File(Data::new(name, size)),
+                ),
+                preceded(
+                    tag("dir "),
+                    map(alphanumeric0, |x| Listing::Directory(Data::new(x, 0))),
+                ),
+            )),
+        )(s)
+    }
+}
+
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 enum Command<'a> {
     GoToRoot,
@@ -51,152 +68,179 @@ enum Command<'a> {
     List(Vec<Listing<'a>>),
 }
 
-fn parse_listing(s: &str) -> IResult<&str, Vec<Listing>> {
-    separated_list1(
-        line_ending,
-        alt((
-            map(
-                separated_pair(nom_u32, tag(" "), not_line_ending),
-                |(size, name)| Listing::File(Data::new(name, size)),
-            ),
-            preceded(
-                tag("dir "),
-                map(alphanumeric0, |x| Listing::Directory(Data::new(x, 0))),
-            ),
-        )),
-    )(s)
+impl<'a> Command<'a> {
+    fn parse(s: &str) -> IResult<&str, Command> {
+        preceded(
+            tag("$ "),
+            alt((
+                preceded(
+                    tag("cd "),
+                    map(not_line_ending, |x| match x {
+                        ".." => Command::GoUp,
+                        "/" => Command::GoToRoot,
+                        _ => Command::ChangeDir(x),
+                    }),
+                ),
+                preceded(
+                    tag("ls"),
+                    map(preceded(line_ending, Listing::parse), Command::List),
+                ),
+            )),
+        )(s)
+    }
+
+    fn parse_all(s: &str) -> Vec<Command> {
+        separated_list1(line_ending, Command::parse)(s).unwrap().1
+    }
 }
 
-fn parse_command(s: &str) -> IResult<&str, Command> {
-    preceded(
-        tag("$ "),
-        alt((
-            preceded(
-                tag("cd "),
-                map(not_line_ending, |x| match x {
-                    ".." => Command::GoUp,
-                    "/" => Command::GoToRoot,
-                    _ => Command::ChangeDir(x),
-                }),
-            ),
-            preceded(
-                tag("ls"),
-                map(preceded(line_ending, parse_listing), |listing| {
-                    Command::List(listing)
-                }),
-            ),
-        )),
-    )(s)
+struct FileSystem<'a> {
+    graph: Graph<Listing<'a>, ()>,
+    root: NodeIndex,
 }
 
-fn parse_lines(s: &str) -> Vec<Command> {
-    separated_list1(line_ending, parse_command)(s).unwrap().1
-}
+impl<'a> FileSystem<'a> {
+    const TOTAL: u32 = 70_000_000;
 
-fn build_filesystem<'a>(commands: &'a [Command]) -> (Graph<Listing<'a>, ()>, NodeIndex) {
-    let mut filesystem: Graph<Listing, ()> = Graph::new();
-    // gotta add in a slash just so we have the root index for later
-    let root = filesystem.add_node(Listing::Directory(Data::new("/", 0)));
-    let mut current = root;
-    for command in commands {
-        match command {
-            Command::GoToRoot => current = root,
-            Command::GoUp => {
-                current = filesystem
-                    .neighbors_directed(current, Incoming)
-                    .next()
-                    .unwrap();
-            }
-            Command::ChangeDir(dir) => {
-                let mut neighbors = filesystem.neighbors_directed(current, Outgoing);
-                // find the outgoing directory
-                match neighbors
-                    .find(|idx| filesystem[*idx] == Listing::Directory(Data { name: dir, size: 0 }))
-                {
-                    // if it exists, set the current
-                    Some(dir_idx) => current = dir_idx,
-                    //otherwise create it and set the current
-                    None => {
-                        let new_idx = filesystem.add_node(Listing::Directory(Data::new(dir, 0)));
-                        filesystem.add_edge(current, new_idx, ());
-                        current = new_idx;
-                    }
-                };
-            }
-            Command::List(children) => {
-                for child in children {
-                    let mut neighbors = filesystem.neighbors_directed(current, Outgoing);
-                    match neighbors.find(|idx| filesystem[*idx] == *child) {
-                        Some(dir_idx) => {}
+    fn build(commands: &'a [Command]) -> FileSystem<'a> {
+        let mut graph: Graph<Listing, ()> = Graph::new();
+        // gotta add in a slash just so we have the root index for later
+        let root = graph.add_node(Listing::Directory(Data::new("/", 0)));
+
+        let mut current = root;
+        for command in commands {
+            match command {
+                Command::GoToRoot => current = root,
+                Command::GoUp => {
+                    current = graph.neighbors_directed(current, Incoming).next().unwrap();
+                }
+                Command::ChangeDir(dir) => {
+                    let mut neighbors = graph.neighbors_directed(current, Outgoing);
+                    // find the outgoing directory
+                    match neighbors
+                        .find(|idx| graph[*idx] == Listing::Directory(Data { name: dir, size: 0 }))
+                    {
+                        // if it exists, set the current
+                        Some(dir_idx) => current = dir_idx,
+                        //otherwise create it and set the current
                         None => {
-                            let new_idx = filesystem.add_node(*child);
-                            filesystem.add_edge(current, new_idx, ());
+                            let new_idx = graph.add_node(Listing::Directory(Data::new(dir, 0)));
+                            graph.add_edge(current, new_idx, ());
+                            current = new_idx;
                         }
                     };
                 }
+                Command::List(children) => {
+                    // go through the children in the listing
+                    for child in children {
+                        // find all the outgoing neighbors
+                        let mut neighbors = graph.neighbors_directed(current, Outgoing);
+                        match neighbors.find(|idx| graph[*idx] == *child) {
+                            // this is here just in case there's a double list
+                            Some(_) => {}
+                            // this node hasn't been found yet, so add it
+                            None => {
+                                let new_idx = graph.add_node(*child);
+                                graph.add_edge(current, new_idx, ());
+                            }
+                        };
+                    }
+                }
             }
         }
+        let mut fs = FileSystem { graph, root };
+        fs.calculate_sizes();
+        fs
     }
-    (filesystem, root)
-}
 
-fn calculate_sizes<'a>(filesystem: &mut Graph<Listing<'a>, ()>, root: NodeIndex) {
-    filesystem.reverse();
-
-    let t = toposort(&*filesystem, None).unwrap();
-    for idx in t {
-        if idx == root {
-            continue;
-        }
-        let parent_idx = filesystem.neighbors(idx).next().unwrap();
-        let node = filesystem[idx];
-        let parent = &mut filesystem[parent_idx];
-        let current_size = match node {
+    fn free_space(&self) -> u32 {
+        let used = match self.graph[self.root] {
             Listing::Directory(data) => data.size,
             Listing::File(data) => data.size,
         };
+        FileSystem::TOTAL - used
+    }
 
-        match parent {
-            Listing::Directory(data) => data.size += current_size,
-            _ => panic!(),
-        };
+    fn calculate_sizes(&mut self) {
+        let graph = &mut self.graph;
+        graph.reverse();
+
+        let t = toposort(&*graph, None).unwrap();
+        for idx in t {
+            if idx == self.root {
+                continue;
+            }
+
+            let node = graph[idx];
+            let current_size = match node {
+                Listing::Directory(data) => data.size,
+                Listing::File(data) => data.size,
+            };
+
+            // add our size to the parent size
+            let parent_idx = graph.neighbors(idx).next().unwrap();
+            let parent = &mut graph[parent_idx];
+            match parent {
+                Listing::Directory(data) => data.size += current_size,
+                _ => panic!("adding size to a file makes no sense"),
+            };
+        }
+    }
+
+    fn get_directories(&self) -> Vec<Listing<'a>> {
+        let t = toposort(&self.graph, None).unwrap();
+        t.iter()
+            .filter_map(|idx| {
+                let node = self.graph[*idx];
+                match node {
+                    listing @ Listing::Directory(_) => Some(listing),
+                    _ => None,
+                }
+            })
+            .collect()
     }
 }
 
 fn problem1(commands: &[Command]) -> u32 {
-    let (mut filesystem, root) = build_filesystem(commands);
-    calculate_sizes(&mut filesystem, root);
-
-    let t = toposort(&filesystem, None).unwrap();
-    let size = t
+    let filesystem = FileSystem::build(commands);
+    filesystem
+        .get_directories()
         .iter()
-        .filter_map(|idx| {
-            let node = filesystem[*idx];
-            match node {
-                Listing::Directory(data) if data.size <= 100_000 => Some(data.size),
-                _ => None,
-            }
+        .filter_map(|listing| match listing {
+            Listing::Directory(data) if data.size <= 100_000 => Some(data.size),
+            _ => None,
         })
-        .sum();
-
-    size
+        .sum()
 }
 
-fn problem2(lines: &[Command]) -> u32 {
-    0
+fn problem2(commands: &[Command]) -> u32 {
+    let needed = 30_000_000;
+
+    let filesystem = FileSystem::build(commands);
+    let free = filesystem.free_space();
+
+    let mut dirs: Vec<u32> = filesystem
+        .get_directories()
+        .iter()
+        .filter_map(|listing| match listing {
+            Listing::Directory(data) if free + data.size > needed => Some(data.size),
+            _ => None,
+        })
+        .collect();
+    dirs.sort();
+
+    dirs[0]
 }
 
 #[cfg(test)]
 mod test {
     use common::test::get_raw_input;
 
-    use crate::{
-        parse_command, parse_lines, parse_listing, problem1, problem2, Command, Data, Listing,
-    };
+    use crate::{problem1, problem2, Command, Data, Listing};
     #[test]
     fn first() {
         let lines = get_raw_input();
-        let input = parse_lines(&lines);
+        let input = Command::parse_all(&lines);
         let result = problem1(&input);
         assert_eq!(result, 95437)
     }
@@ -204,13 +248,13 @@ mod test {
     #[test]
     fn second() {
         let lines = get_raw_input();
-        let input = parse_lines(&lines);
+        let input = Command::parse_all(&lines);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 24933642)
     }
     #[test]
     fn ls() {
-        let ls = parse_command(
+        let ls = Command::parse(
             r#"$ ls
 dir foo
 1234 foo.txt"#,
@@ -226,22 +270,22 @@ dir foo
 
     #[test]
     fn cd() {
-        let cd = parse_command("$ cd ..").unwrap().1;
+        let cd = Command::parse("$ cd ..").unwrap().1;
         assert_eq!(cd, Command::GoUp);
 
-        let cd = parse_command("$ cd /").unwrap().1;
+        let cd = Command::parse("$ cd /").unwrap().1;
         assert_eq!(cd, Command::GoToRoot);
 
-        let cd = parse_command("$ cd foo").unwrap().1;
+        let cd = Command::parse("$ cd foo").unwrap().1;
         assert_eq!(cd, Command::ChangeDir("foo"));
     }
 
     #[test]
     fn listing() {
-        let listing = parse_listing("dir foo").unwrap().1;
+        let listing = Listing::parse("dir foo").unwrap().1;
         assert_eq!(listing, vec![Listing::Directory(Data::new("foo", 0))]);
 
-        let listing = parse_listing(
+        let listing = Listing::parse(
             r#"1234 foo.txt
 dir foo
 5678 bar.txt"#,
