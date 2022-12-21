37d66abb40c311a9d21180814d0c94d3d7736059
David Stone <david@stabbylambda.com>
2022-12-20 20:01:39 -0800

    Faster thanks to reddit, but still slow

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -20,40 +20,6 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(
-        newline,
-        map(
-            tuple((
-                (delimited(tag("Blueprint "), nom_u32, tag(": "))),
-                (delimited(tag("Each ore robot costs "), nom_u32, tag(" ore. "))),
-                (delimited(tag("Each clay robot costs "), nom_u32, tag(" ore. "))),
-                (tuple((
-                    delimited(tag("Each obsidian robot costs "), nom_u32, tag(" ore ")),
-                    delimited(tag("and "), nom_u32, tag(" clay. ")),
-                ))),
-                (tuple((
-                    delimited(tag("Each geode robot costs "), nom_u32, tag(" ore ")),
-                    delimited(tag("and "), nom_u32, tag(" obsidian.")),
-                ))),
-            )),
-            |(id, ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs))| {
-                Blueprint::new(
-                    id,
-                    [
-                        [ore, 0, 0, 0],
-                        [clay, 0, 0, 0],
-                        [obs_ore, obs_clay, 0, 0],
-                        [geode_ore, 0, geode_obs, 0],
-                    ],
-                )
-            },
-        ),
-    )(input);
-
-    result.unwrap().1
-}
-
 type Input = Vec<Blueprint>;
 type Costs = [[u32; 4]; 4];
 #[derive(Debug)]
@@ -91,7 +57,7 @@ struct Inventory {
     time_left: u32,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Types {
     Ore = 0,
     Clay = 1,
@@ -108,9 +74,17 @@ impl Inventory {
         }
     }
 
-    fn gather(&mut self, times: u32) {
-        for n in 0..4 {
-            self.items[n] += self.bots[n] * times;
+    fn gather(&self) -> Inventory {
+        let new_items = (0..4)
+            .map(|n| self.items[n] + self.bots[n])
+            .collect::<Vec<u32>>()
+            .try_into()
+            .unwrap();
+
+        Inventory {
+            items: new_items,
+            bots: self.bots,
+            time_left: self.time_left - 1,
         }
     }
 
@@ -125,6 +99,14 @@ impl Inventory {
                 .all(|(&cost, items)| cost <= items)
     }
 
+    fn items_remaining(&self, robot_type: Types) -> u32 {
+        let idx = robot_type as usize;
+        let item_count = self.items[idx];
+        let bot_collection_count = self.bots[idx] * self.time_left;
+
+        item_count + bot_collection_count
+    }
+
     fn build_robot(&mut self, bp: &Blueprint, robot_type: Types) {
         self.bots[robot_type as usize] += 1;
 
@@ -132,66 +114,116 @@ impl Inventory {
             self.items[idx] -= bp.costs[robot_type as usize][idx];
         }
     }
+
+    fn best_possible(&self, robot_type: Types) -> u32 {
+        let items_remaining = self.items_remaining(robot_type);
+        let bots_to_be_added = self.time_left * (self.time_left - 1) / 2;
+
+        items_remaining + bots_to_be_added
+    }
 }
 
-fn best_possible(inventory: &Inventory, idx: usize) -> u32 {
-    let item_count = inventory.items[idx];
-    let bot_collection_count = inventory.bots[idx] * inventory.time_left;
-    let bots_to_be_added = inventory.time_left * (inventory.time_left - 1) / 2;
+fn parse(input: &str) -> Input {
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        map(
+            tuple((
+                (delimited(tag("Blueprint "), nom_u32, tag(": "))),
+                (delimited(tag("Each ore robot costs "), nom_u32, tag(" ore. "))),
+                (delimited(tag("Each clay robot costs "), nom_u32, tag(" ore. "))),
+                (tuple((
+                    delimited(tag("Each obsidian robot costs "), nom_u32, tag(" ore ")),
+                    delimited(tag("and "), nom_u32, tag(" clay. ")),
+                ))),
+                (tuple((
+                    delimited(tag("Each geode robot costs "), nom_u32, tag(" ore ")),
+                    delimited(tag("and "), nom_u32, tag(" obsidian.")),
+                ))),
+            )),
+            |(id, ore, clay, (obs_ore, obs_clay), (geode_ore, geode_obs))| {
+                Blueprint::new(
+                    id,
+                    [
+                        [ore, 0, 0, 0],
+                        [clay, 0, 0, 0],
+                        [obs_ore, obs_clay, 0, 0],
+                        [geode_ore, 0, geode_obs, 0],
+                    ],
+                )
+            },
+        ),
+    )(input);
 
-    item_count + bot_collection_count + bots_to_be_added
+    result.unwrap().1
 }
 
-fn simulate(inventory: Inventory, bp: &Blueprint, best_so_far: u32) -> u32 {
-    // just stop if there's no more time left, these are the geodes we have plus the ones the bots will collect
-    if inventory.time_left == 1 {
-        let geodes = inventory.items[Types::Geode as usize] + inventory.bots[Types::Geode as usize];
-        return geodes;
+fn simulate(
+    inventory: Inventory,
+    bp: &Blueprint,
+    best_so_far: u32,
+    previous_skip: &[Types],
+) -> u32 {
+    // If there's no more time left, or we can't possibly create another geode bot, we need to bail
+    let needed_obsidian = bp.max_costs[Types::Obsidian as usize];
+    if inventory.time_left == 1 || inventory.best_possible(Types::Obsidian) < needed_obsidian {
+        return inventory.items_remaining(Types::Geode);
     }
 
     // this branch is trash, just get out of it
-    if best_possible(&inventory, Types::Geode as usize) < best_so_far {
+    if inventory.best_possible(Types::Geode) < best_so_far {
         return 0;
     }
 
-    let mut new_inventory = inventory.clone();
-    let mut best = best_so_far;
-
-    // gather items and burn time
-    new_inventory.time_left -= 1;
-    new_inventory.gather(1);
-
-    // we're going to try doing geode first because that's the most important one to find
-    for robot_type in [Types::Geode, Types::Ore, Types::Clay, Types::Obsidian] {
-        if !inventory.can_build(bp, robot_type) {
-            continue;
-        }
-
-        let mut branch_with_new_robot = new_inventory.clone();
-        branch_with_new_robot.build_robot(bp, robot_type);
-        let score = simulate(branch_with_new_robot, bp, best);
-        best = score.max(best);
+    // if we can build geode, this is the only logical path, ignore the rest of the robot types
+    if inventory.can_build(bp, Types::Geode) {
+        let mut new_inventory = inventory.gather();
+        new_inventory.build_robot(bp, Types::Geode);
+        return simulate(new_inventory, bp, best_so_far, &[]);
     }
 
-    // I guess...we do nothing?
-    let score = simulate(new_inventory, bp, best);
-    best = score.max(best);
+    let remaining = [Types::Ore, Types::Clay, Types::Obsidian];
+    let can_build: Vec<Types> = remaining
+        .into_iter()
+        .filter(|t| inventory.can_build(bp, *t))
+        .filter(|t| !previous_skip.contains(t)) // prune branches where we tried to
+        .collect();
 
-    best
+    let best = can_build
+        .par_iter()
+        .map(|robot_type| {
+            let mut new_inventory = inventory.gather();
+            new_inventory.build_robot(bp, *robot_type);
+            simulate(new_inventory, bp, best_so_far, &[])
+        })
+        .max()
+        .unwrap_or(best_so_far)
+        .max(best_so_far);
+
+    // Worst case scenario, just gather items
+    simulate(inventory.gather(), bp, best, &can_build[..]).max(best)
 }
 
-fn get_blueprint_score(bp: &Blueprint) -> u32 {
-    let i = Inventory::new(24);
-    bp.id * simulate(i, bp, 0)
-}
-
-const MINUTES: u32 = 24;
 fn problem1(input: &Input) -> u32 {
-    input.par_iter().map(get_blueprint_score).sum()
+    let max_time = 24;
+    input
+        .par_iter()
+        .map(|bp| {
+            let i = Inventory::new(max_time);
+            bp.id * simulate(i, bp, 0, &[])
+        })
+        .sum()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> u32 {
+    let max_time = 32;
+    input
+        .par_iter()
+        .take(3)
+        .map(|bp| {
+            let i = Inventory::new(max_time);
+            simulate(i, bp, 0, &[])
+        })
+        .product()
 }
 
 #[cfg(test)]
@@ -208,11 +240,10 @@ mod test {
     }
 
     #[test]
-    #[ignore = "reason"]
     fn second() {
         let input = get_raw_input();
         let input = parse(&input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 56 * 62)
     }
 }
