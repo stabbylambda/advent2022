7e0226844d995123295c8a9e707308483f6d4e40
David Stone <david@stabbylambda.com>
2022-12-22 22:46:31 -0800

    Day 22 part 2 hard coded for 50x50

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,6 +1,5 @@
 use common::get_raw_input;
 use ndarray::prelude::*;
-use parsing::parse_grid;
 
 use crate::parsing::parse;
 
@@ -97,14 +96,13 @@ impl Position {
         }
     }
 
-    fn rotate(&mut self, instruction: &Instruction) -> Heading {
+    fn rotate(&mut self, instruction: &Instruction) {
         let x = match instruction {
             Instruction::TurnLeft => 3,
             Instruction::TurnRight => 1,
             _ => 0,
         };
         self.heading = (self.heading as u32 + x).try_into().unwrap();
-        self.heading
     }
 
     fn get_password(&self) -> u32 {
@@ -116,7 +114,7 @@ impl Position {
         first + second + third
     }
 
-    fn walk(&mut self, steps: u32, grid: &Array2<Space>) {
+    fn walk(&mut self, steps: u32, grid: &Array2<Space>, void_treatment: VoidTreatment) {
         // get the correct axis to look at and slice the array on that axis
         let (axis, idx, rev) = match self.heading {
             Heading::Up => (Axis(1), 0, true),
@@ -126,7 +124,6 @@ impl Position {
         };
 
         let slice = grid.index_axis(axis, self.coords[axis.0]);
-        dbg!(&slice);
 
         let mut s = 0u32;
         while s < steps {
@@ -137,109 +134,108 @@ impl Position {
             match slice.get(next) {
                 Some(Space::Wall) => break,
                 Some(Space::Empty) => {
-                    // println!(
-                    //     "We can move 1 {:?} to ({}, {})",
-                    //     player_position.heading,
-                    //     player_position.coords[0],
-                    //     player_position.coords[1]
-                    // );
                     s += 1;
                     self.coords[idx] = next;
                 }
                 Some(Space::Void) => {
-                    let next_non_void = if rev {
-                        slice
-                            .iter()
-                            .rev()
-                            .cycle()
-                            .find(|&&x| x != Space::Void)
-                            .unwrap()
-                    } else {
-                        slice.iter().cycle().find(|&&x| x != Space::Void).unwrap()
-                    };
+                    match void_treatment {
+                        VoidTreatment::TwoD => {
+                            let next_non_void = if rev {
+                                slice
+                                    .iter()
+                                    .rev()
+                                    .cycle()
+                                    .find(|&&x| x != Space::Void)
+                                    .unwrap()
+                            } else {
+                                slice.iter().cycle().find(|&&x| x != Space::Void).unwrap()
+                            };
 
-                    if *next_non_void == Space::Wall {
-                        break;
+                            if *next_non_void == Space::Wall {
+                                break;
+                            }
+
+                            // is the next non-void space a wall?
+                            self.coords[idx] = next;
+                        }
+                        VoidTreatment::ThreeD => {
+                            /* when we hit a void on the map, but the map is in 3d mode, we need to translate
+                            ourselves off this cube edge and onto another, we also still need to check that
+                            the next space isn't a wall */
+
+                            // translate our coords and heading
+                            let (next_row, next_col, next_heading) = self.translate_to_3d();
+
+                            if grid[[next_row, next_col]] == Space::Wall {
+                                break;
+                            }
+
+                            self.coords = [next_row, next_col];
+                            self.heading = next_heading;
+
+                            // complete the rest of the walk
+                            self.walk(steps - s - 1, grid, void_treatment);
+                            return;
+                        }
                     }
-
-                    // is the next non-void space a wall?
-                    self.coords[idx] = next;
                 }
-                None => todo!(),
+                None => unreachable!(),
             }
+        }
+    }
+
+    fn get_face(&self) -> usize {
+        let row = self.coords[0];
+        let col = self.coords[1];
+
+        match (row, col) {
+            (0..=49, 50..=99) => 1,
+            (0..=49, 100..=149) => 2,
+            (50..=99, 50..=99) => 3,
+            (100..=149, 50..=99) => 4,
+            (100..=149, 0..=49) => 5,
+            (150..=199, 0..=49) => 6,
+
+            _ => panic!(),
+        }
+    }
+
+    fn translate_to_3d(&mut self) -> (usize, usize, Heading) {
+        let row = self.coords[0];
+        let col = self.coords[1];
+
+        let heading = self.heading;
+        let face = self.get_face();
+
+        match (face, heading, row, col) {
+            (1, Heading::Up, 0, _) => (150 + (col - 50), 0, Heading::Right),
+            (1, Heading::Left, _, 50) => (149 - row, 0, Heading::Right),
+
+            (2, Heading::Up, 0, _) => (199, col - 100, Heading::Up),
+            (2, Heading::Right, _, 149) => (149 - row, 99, Heading::Left),
+            (2, Heading::Down, 49, _) => (50 + (col - 100), 99, Heading::Left),
+
+            (3, Heading::Left, _, 50) => (100, row - 50, Heading::Down),
+            (3, Heading::Right, _, 99) => (49, 100 + (row - 50), Heading::Up),
+
+            (4, Heading::Right, _, 99) => (49 - (row - 100), 149, Heading::Left),
+            (4, Heading::Down, 149, _) => (150 + (col - 50), 49, Heading::Left),
+
+            (5, Heading::Left, _, 0) => (49 - (row - 100), 50, Heading::Right),
+            (5, Heading::Up, 100, _) => (50 + col, 50, Heading::Right),
+
+            (6, Heading::Left, _, 0) => (0, 50 + (row - 150), Heading::Down),
+            (6, Heading::Down, 199, _) => (0, 100 + col, Heading::Down),
+            (6, Heading::Right, _, 49) => (149, 50 + (row - 150), Heading::Up),
+
+            _ => panic!(),
         }
     }
 }
 
-#[test]
-#[ignore = "reason"]
-fn walk_up() {
-    let map = ". 
-# 
-..
-.#
-..";
-
-    let grid = parse_grid(map).unwrap().1;
-
-    let mut p = Position {
-        coords: [0, 0],
-        heading: Heading::Up,
-    };
-
-    // can walk over the top of the map
-    p.walk(5, &grid);
-    assert_eq!(p.coords, [2, 0]);
-
-    // now move over one
-    p.coords = [2, 1];
-
-    // can walk over the top, over a void, and hit a wall
-    p.walk(5, &grid);
-    assert_eq!(p.coords, [4, 1]);
-}
-
-#[test]
-fn walk_left() {
-    let map = "  .#.
-  ..#";
-
-    let grid = parse_grid(map).unwrap().1;
-
-    let mut p = Position {
-        coords: [0, 2],
-        heading: Heading::Left,
-    };
-
-    // // can walk over void left
-    // p.walk(3, &grid);
-    // assert_eq!(p.coords, [0, 4]);
-
-    p.coords = [1, 3];
-
-    p.walk(10, &grid);
-    print_map(&grid, &p);
-    assert_eq!(p.coords, [1, 2]);
-}
-
-#[test]
-#[ignore = "reason"]
-fn rotate() {
-    let mut p = Position {
-        coords: [0, 0],
-        heading: Heading::Up,
-    };
-
-    let x: Vec<Heading> = (0..4).map(|x| p.rotate(&Instruction::TurnLeft)).collect();
-    assert_eq!(
-        x,
-        vec![Heading::Left, Heading::Down, Heading::Right, Heading::Up]
-    );
-    let x: Vec<Heading> = (0..4).map(|x| p.rotate(&Instruction::TurnRight)).collect();
-    assert_eq!(
-        x,
-        vec![Heading::Right, Heading::Down, Heading::Left, Heading::Up]
-    );
+enum VoidTreatment {
+    TwoD,
+    ThreeD,
 }
 
 fn problem1(input: &Input) -> u32 {
@@ -247,30 +243,39 @@ fn problem1(input: &Input) -> u32 {
     let mut player_position = Position::get_start(grid);
 
     for x in moves.iter() {
-        println!("{x:?}");
         match x {
             Instruction::TurnLeft | Instruction::TurnRight => {
                 player_position.rotate(x);
             }
-            Instruction::Walk(steps) => player_position.walk(*steps, grid),
+            Instruction::Walk(steps) => player_position.walk(*steps, grid, VoidTreatment::TwoD),
         }
-        // print_map(&grid, &player_position);
     }
 
     player_position.get_password()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> u32 {
+    let (grid, moves) = input;
+    let mut player_position = Position::get_start(grid);
+
+    for x in moves.iter() {
+        match x {
+            Instruction::TurnLeft | Instruction::TurnRight => {
+                player_position.rotate(x);
+            }
+            Instruction::Walk(steps) => player_position.walk(*steps, grid, VoidTreatment::ThreeD),
+        }
+    }
+
+    player_position.get_password()
 }
 
 #[cfg(test)]
 mod test {
     use common::test::get_raw_input;
 
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, parsing::parse_grid, problem1, problem2, Heading, Position};
     #[test]
-    #[ignore = "reason"]
     fn first() {
         let input = get_raw_input();
         let input = parse(&input);
@@ -279,11 +284,57 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn second() {
         let input = get_raw_input();
         let input = parse(&input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 5031)
+    }
+    #[test]
+    fn walk_up() {
+        let map = ". 
+# 
+..
+.#
+..";
+
+        let grid = parse_grid(map).unwrap().1;
+
+        let mut p = Position {
+            coords: [0, 0],
+            heading: Heading::Up,
+        };
+
+        // can walk over the top of the map
+        p.walk(5, &grid, crate::VoidTreatment::TwoD);
+        assert_eq!(p.coords, [2, 0]);
+
+        // now move over one
+        p.coords = [2, 1];
+
+        // can walk over the top, over a void, and hit a wall
+        p.walk(5, &grid, crate::VoidTreatment::TwoD);
+        assert_eq!(p.coords, [4, 1]);
+    }
+
+    #[test]
+    fn walk_left() {
+        let map = "  .#.
+  ..#";
+
+        let grid = parse_grid(map).unwrap().1;
+
+        let mut p = Position {
+            coords: [0, 2],
+            heading: Heading::Left,
+        };
+
+        // can walk over void left
+        p.walk(3, &grid, crate::VoidTreatment::TwoD);
+        assert_eq!(p.coords, [0, 4]);
+
+        p.coords = [1, 3];
+        p.walk(10, &grid, crate::VoidTreatment::TwoD);
+        assert_eq!(p.coords, [1, 2]);
     }
 }
